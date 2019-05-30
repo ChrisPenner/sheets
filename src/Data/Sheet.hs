e054521e4dcd6c695c720b112c902cc00a903c10
Chris Penner <christopher.penner@gmail.com>
2019-05-30 12:20:52 +0200

    Compiling specialized to Rows

--- src/Data/Sheet.hs
+++ src/Data/Sheet.hs
@@ -23,8 +23,8 @@ import GHC.TypeNats
 import Data.Kind
 
 data Sheet columns a where
- (:+:) :: x a -> Sheet xs a -> Sheet (x:xs) a
- FinalCol :: x a -> Sheet '[x] a
+ (:+:) :: Rows k sz a -> Sheet xs a -> Sheet (Rows k sz : xs) a
+ FinalCol :: Rows k sz a -> Sheet '[Rows k sz] a
 
 infix 6 :+:
 
@@ -32,7 +32,7 @@ data Ref columns where
  Y :: Rep x -> Ref (x:xs)
  N :: Ref xs -> Ref (x:xs)
 
-instance (AllC Functor cols) => Functor (Sheet cols) where
+instance Functor (Sheet cols) where
   fmap f (FinalCol x) = FinalCol $ fmap f x
   fmap f (fa :+: rest) = fmap f fa :+: (fmap f rest)
 
@@ -44,15 +44,10 @@ class (forall a. Show a => Show (f a)) => ShowX f
 
 deriving instance (AllC ShowX cols, Show a) => Show (Sheet cols a)
 
--- data Size =
---   Dyn | Sz Int
-
-data Rows k sz a where
-  -- DynRows :: [a] -> Rows k Nothing a
-  Rows :: V.Vector n a -> Rows k n a
+data Rows k n a = Rows (V.Vector n a)
+  deriving stock Show
   deriving anyclass ShowX
-
-deriving instance (Show a) => Show (Rows k sz a)
+-- deriving instance (Show a) => Show (Rows k sz a)
 
 instance Functor (Rows k sz) where
   fmap f (Rows v) = Rows $ fmap f v
@@ -66,68 +61,39 @@ instance (KnownNat n) => Representable (Rows k n) where
   tabulate f = Rows $ V.generate f
 
 
-instance ( AllC Functor (x : y : xs)
-         , AllC Representable (x : y : xs)
-         , Representable (Sheet (y : xs))
-         , Rep (Sheet (y : xs)) ~ Ref (y : xs)
-         ) => Distributive (Sheet (x : y : xs)) where
+instance (KnownNat sz, Representable (Sheet xs), Rep (Sheet xs) ~ Ref xs)
+    => Distributive (Sheet (Rows j sz : xs)) where
     distribute = distributeRep
 
-instance ( AllC Functor (x : y : xs)
-         , AllC Representable (x : y : xs)
-         , Rep (Sheet (y : xs)) ~ Ref (y : xs)
-         , (Representable (Sheet (y : xs)))
-         ) => Representable (Sheet (x : y : xs)) where
-    type Rep (Sheet (x : y : xs)) = Ref (x : y : xs)
+instance (KnownNat sz, Representable (Sheet xs), Rep (Sheet xs) ~ Ref xs)
+    => Representable (Sheet (Rows j sz : xs)) where
+    type Rep (Sheet (Rows j sz : xs)) = Ref (Rows j sz : xs)
 
-    index (x :+: xs) (Y r) = index x r
-    index (x :+: xs) (N r) = index xs r
+    index (Rows x :+: xs) (Y r) = index x r
+    index (_ :+: xs) (N r) = index xs r
     tabulate f = tabulate (f . down) :+: tabulate (f . up)
       where
-        down :: Rep x -> Ref (x : y : xs)
         down = Y
-        up :: Ref (y : xs) -> Ref (x : y : xs)
         up = N
 
-instance (Functor x, Representable x) => Distributive (Sheet '[x]) where
+instance {-# OVERLAPPING #-} (KnownNat sz) => Distributive (Sheet '[Rows k sz]) where
     distribute = distributeRep
 
-instance (Representable x) => Representable (Sheet '[x]) where
-    type Rep (Sheet '[x]) = Ref '[x]
+instance {-# OVERLAPPING #-} (KnownNat sz) => Representable (Sheet '[Rows k sz]) where
+    type Rep (Sheet '[Rows k sz]) = Ref '[ Rows k sz ]
 
-    index (x :+: xs) (Y r) = index x r
+    index (FinalCol x) (Y r) = index x r
     tabulate f = FinalCol $ tabulate (f . Y)
-      where
-        down :: Rep x -> Ref (x : y : xs)
-        down = Y
-        up :: Ref (y : xs) -> Ref (x : y : xs)
-        up = N
-
--- instance Representable (Sheet '[x]) where
---   type Rep (Sheet cs) = Ref cs
---   index (FinalCol x) (FinalRef r) = index x r
---   tabulate f = Rows $ V.generate f
-
-
-
-
--- instance Representable (Rows k Nothing) where
---   type Rep (Rows k Nothing) = Int
---   index (DynRows xs) n = xs !! n
---   tabulate f = DynRows $ generate (f . fromIntegral . getFinite)
-
-
-
 
 data Items
 
-s :: Sheet '[Rows Items 3] Char
-s = tabulate go
-  where
-    go :: Ref '[Rows Items 3] -> Char
-    go (Y 0) = 'A'
-    go (Y 1) = 'B'
-    go (Y 2) = 'C'
+-- s :: Sheet '[Rows Items 3] Char
+-- s = tabulate go
+--   where
+--     go :: Ref '[Rows Items 3] -> Char
+--     go (Y 0) = 'A'
+--     go (Y 1) = 'B'
+--     go (Y 2) = 'C'
 
-r :: Sheet '[Rows Items 3] Char
-r = FinalCol $ Rows (V.fromTuple ('a', 'b', 'c'))
+-- r :: Sheet '[Rows Items 3] Char
+-- r = FinalCol $ Rows (V.fromTuple ('a', 'b', 'c'))
